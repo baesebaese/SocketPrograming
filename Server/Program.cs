7b3db5f4e4cf8ec3fcf477cdc2d3bacefb8b476f
morendo <morendo90@gmail.com>
2023-07-19 19:47:17 +0900

    serverSocket 생성

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server;
+
+internal class Program 
+{
+    static void Main(string[] args) 
+    {
+        // param: 주소체계, 소켓타입
+        // Stream 은 연결 지향을 의미
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        //엔드 포인트
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("192.168.30.1"), 20000);
+        
+        //serverSocket에 ip와 port 할당
+        serverSocket.Bind(endPoint);
+
+        // Listen의 역할
+        // 1. 클라이언트들의 연결 요청을 대기상태로 변경
+        // 2. 백로그큐 생성 => 클라이언트들의 연결 요청 대기실
+        //   최대 20개의 클라이언트 연결요청을 대기할 수 있다.
+        serverSocket.Listen(20);
+
+
+    }
+}
